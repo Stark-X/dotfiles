d56dddda1876c82d66283aac5b4927fd8493fd4c
Stark-X <13653522+Stark-X@users.noreply.github.com>
2018-08-01 15:32:25 +0800

    Add HammerSpoon for Windows switch

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,74 @@
+-- Auto reload config
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config reloaded")
+
+-- Hints
+hs.hints.style="vimperator"
+hs.hotkey.bind({"cmd",}, 'escape', function()
+    -- API: hs.hints.windowHints([windows, callback, allowNonStandard])
+    hs.hints.windowHints(nil, nil, false)
+end)
+
+-- Window resizing
+hs.hotkey.bind({"cmd", "alt",}, "h", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt",}, "l", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt",}, "k", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt",}, "j", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + max.h / 2
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
