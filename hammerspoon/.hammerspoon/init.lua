15fe9d5131638bd7047d6e32b5935d99d9845885
Stark-X <13653522+Stark-X@users.noreply.github.com>
2019-04-19 17:16:29 +0800

    [hammerspoon] replace Karabiner

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -226,3 +226,38 @@ hs.hotkey.bind({"shift",}, 'space', function()
     hs.grid.show()
 end)
 
+
+REPEAT_DELAY = 200
+
+-- Delete a word before the cursor
+fn_delete_a_world = function()
+    hs.eventtap.keyStroke('{"alt", }', 'delete', REPEAT_DELAY)
+end
+hs.hotkey.bind('{"ctrl",}', 'w', fn_delete_a_world, nil, fn_delete_a_world)
+
+-- Map ctrl + [ to escape
+fn_escape = function()
+    hs.eventtap.keyStroke(nil, 'escape')
+end
+hs.hotkey.bind('{"ctrl",}', '[', fn_escape, nil, fn_escape)
+
+-- simulate vim mode
+fn_left = function()
+    hs.eventtap.keyStroke(nil, 'left', REPEAT_DELAY)
+end
+hs.hotkey.bind('{"ctrl",}', 'h', fn_left, nil, fn_left)
+
+fn_down = function()
+    hs.eventtap.keyStroke(nil, 'down', REPEAT_DELAY)
+end
+hs.hotkey.bind('{"ctrl",}', 'j', fn_down, nil, fn_down)
+
+fn_up = function()
+    hs.eventtap.keyStroke(nil, 'up', REPEAT_DELAY)
+end
+hs.hotkey.bind('{"ctrl",}', 'k', fn_up, nil, fn_up)
+
+fn_right = function()
+    hs.eventtap.keyStroke(nil, 'right', REPEAT_DELAY)
+end
+hs.hotkey.bind('{"ctrl",}', 'l', fn_right, nil, fn_right)
