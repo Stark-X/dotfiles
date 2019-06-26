3a25dd6c0a7af7c1f534db7f4a904e69ddcc7e9f
Stark-X <13653522+Stark-X@users.noreply.github.com>
2019-06-26 11:34:03 +0800

    [hammerspoon] add feature move/resize windows with mouse, emacs binding

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -186,7 +186,7 @@ function mouseHighlight()
             mouseCircleTimer:stop()
         end
     end
-    mousepoint = hs.mouse.get()
+    mousepoint = hs.mouse.getRelativePosition()
     mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
     mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
     mouseCircle:setFill(false)
@@ -230,10 +230,28 @@ end)
 REPEAT_DELAY = 200
 
 -- Delete a word before the cursor
-fn_delete_a_world = function()
+fn_delete_a_word = function()
     hs.eventtap.keyStroke('{"alt", }', 'delete', REPEAT_DELAY)
 end
-hs.hotkey.bind('{"ctrl",}', 'w', fn_delete_a_world, nil, fn_delete_a_world)
+bn_delete_a_word = hs.hotkey.bind('{"ctrl",}', 'w', fn_delete_a_word, nil, fn_delete_a_wold)
+
+function enableBinds()
+    bn_delete_a_word:enable()
+end
+function disableBinds()
+    bn_delete_a_word:disable()
+end
+
+local wf = hs.window.filter
+
+wf_vim = wf.new({'MacVim', 'iTerm2'})
+wf_vim:subscribe(wf.windowFocused, disableBinds)
+wf_vim:subscribe(wf.windowUnfocused, enableBinds)
+
+
+-- TODO: Check-> Warning:   wfilter: No accessibility access to app GoldenDict (no watcher pid)
+-- all_wf = wf.new(true)
+-- all_wf:subscribe(wf.windowFocused, function(win, name, en) logger.d(win, name) end)
 
 -- Map ctrl + [ to escape
 fn_escape = function()
@@ -261,3 +279,64 @@ fn_right = function()
     hs.eventtap.keyStroke(nil, 'right', REPEAT_DELAY)
 end
 hs.hotkey.bind('{"ctrl",}', 'l', fn_right, nil, fn_right)
+
+-- Inspired by Linux alt-drag or Better Touch Tools move/resize functionality
+-- from https://gist.github.com/kizzx2/e542fa74b80b7563045a
+-- Command-shift-move: move window under mouse
+-- Alt-Shift-move: resize window under mouse
+function get_window_under_mouse()
+   local my_pos = hs.geometry.new(hs.mouse.getAbsolutePosition())
+   local my_screen = hs.mouse.getCurrentScreen()
+   return hs.fnutils.find(hs.window.orderedWindows(), function(w)
+                             return my_screen == w:screen() and
+                                w:isStandard() and
+                                (not w:isFullScreen()) and
+                                my_pos:inside(w:frame())
+   end)
+end
+
+dragging = {}                   -- global variable to hold the dragging/resizing state
+
+drag_event = hs.eventtap.new({ hs.eventtap.event.types.mouseMoved }, function(e)
+      if not dragging then return nil end
+      if dragging.mode==3 then -- just move
+         local dx = e:getProperty(hs.eventtap.event.properties.mouseEventDeltaX)
+         local dy = e:getProperty(hs.eventtap.event.properties.mouseEventDeltaY)
+         dragging.win:move({dx, dy}, nil, false, 0)
+      else -- resize
+         local pos=hs.mouse.getAbsolutePosition()
+         local w1 = dragging.size.w + (pos.x-dragging.off.x)
+         local h1 = dragging.size.h + (pos.y-dragging.off.y)
+         dragging.win:setSize(w1, h1)
+      end
+end)
+
+flags_event = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(e)
+      local flags = e:getFlags()
+      local mode=(flags.shift and 1 or 0) + (flags.cmd and 2 or 0) + (flags.alt and 4 or 0)
+      if mode==3 or mode==5 then -- valid modes
+         if dragging then
+            if dragging.mode == mode then return nil end -- already working
+         else
+            -- only update window if we hadn't started dragging/resizing already
+            dragging={win = get_window_under_mouse()}
+            if not dragging.win then -- no good window
+               dragging=nil
+               return nil
+            end
+         end
+         dragging.mode = mode   -- 3=drag, 5=resize
+         if mode==5 then
+            dragging.off=hs.mouse.getAbsolutePosition()
+            dragging.size=dragging.win:size()
+         end
+         drag_event:start()
+      else                      -- not a valid mode
+         if dragging then
+            drag_event:stop()
+            dragging = nil
+         end
+      end
+      return nil
+end)
+flags_event:start()
