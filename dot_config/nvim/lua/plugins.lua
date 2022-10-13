69dc6928b026311f9dc2f1767a38960b36e478fd
Stark-X <13653522+Stark-X@users.noreply.github.com>
2022-10-13 18:47:27 +0800

    [nvim] drop ale

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -24,16 +24,6 @@ return require("packer").startup(function(use)
     -- Load on an autocommand event
     use({ "andymass/vim-matchup", event = "VimEnter" })
 
-    vim.g.ale_disable_lsp = 1
-    -- Load on a combination of conditions: specific filetypes or commands
-    -- Also run code after load (see the "config" key)
-    use({
-        "w0rp/ale",
-        ft = { "sh", "zsh", "bash", "c", "cpp", "cmake", "html", "markdown", "racket", "vim", "tex" },
-        cmd = "ALEEnable",
-        config = "vim.cmd[[ALEEnable]]",
-    })
-
     -- Use specific branch, dependency and run lua file after load
     -- use {
     -- 'glepnir/galaxyline.nvim', branch = 'main', config = function() require'statusline' end,
@@ -44,7 +34,60 @@ return require("packer").startup(function(use)
     use({
         "lewis6991/gitsigns.nvim",
         requires = { "nvim-lua/plenary.nvim" },
-        config = function() require("gitsigns").setup() end,
+        config = function()
+            require("gitsigns").setup({
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+                    delay = 500,
+                    ignore_whitespace = false,
+                },
+                current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map("n", "]c", function()
+                        if vim.wo.diff then
+                            return "]c"
+                        end
+                        vim.schedule(function() gs.next_hunk() end)
+                        return "<Ignore>"
+                    end, { expr = true })
+
+                    map("n", "[c", function()
+                        if vim.wo.diff then
+                            return "[c"
+                        end
+                        vim.schedule(function() gs.prev_hunk() end)
+                        return "<Ignore>"
+                    end, { expr = true })
+
+                    -- Actions
+                    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+                    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+                    map("n", "<leader>hS", gs.stage_buffer)
+                    map("n", "<leader>hu", gs.undo_stage_hunk)
+                    map("n", "<leader>hR", gs.reset_buffer)
+                    map("n", "<leader>hp", gs.preview_hunk)
+                    map("n", "<leader>hb", function() gs.blame_line({ full = true }) end)
+                    map("n", "<leader>tb", gs.toggle_current_line_blame)
+                    map("n", "<leader>hd", gs.diffthis)
+                    map("n", "<leader>hD", function() gs.diffthis("~") end)
+                    map("n", "<leader>td", gs.toggle_deleted)
+
+                    -- Text object
+                    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+                end,
+            })
+        end,
     })
 
     -- You can specify multiple plugins in a single call
@@ -170,8 +213,7 @@ return require("packer").startup(function(use)
         end,
     })
 
-    use("lewis6991/impatient.nvim")
-
+    use({ "lewis6991/impatient.nvim", config = function() require("impatient") end })
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
