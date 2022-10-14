e4808cd32add5e6392734e147c7eda5d770947a0
Stark-X <13653522+Stark-X@users.noreply.github.com>
2022-10-14 15:08:10 +0800

    [nvim] config treesitter

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -92,7 +92,41 @@ return require("packer").startup(function(use)
     })
 
     -- You can specify multiple plugins in a single call
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "cmake", "vim", "bash", "toml", "yaml" },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                },
+                indent = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>",
+                        node_incremental = "<CR>",
+                        node_decremental = "<BS>",
+                        scope_incremental = "<TAB>",
+                    },
+                },
+            })
+
+            vim.o.foldmethod = "expr"
+            vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+        end,
+    })
 
     use({
         "fgheng/winbar.nvim",
