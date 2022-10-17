ddbc2bf4b5cd96d0106e4276cceb82d42857ec2c
Stark-X <13653522+Stark-X@users.noreply.github.com>
2022-10-17 14:11:03 +0800

    [nvim] mark filetype as 'text' by default to avoiding match-up not work issue

--- dot_config/nvim/lua/index.lua
+++ dot_config/nvim/lua/index.lua
@@ -1,3 +1,12 @@
 require("plugins")
 
+-- mark filetype as 'text' by default to avoiding match-up not work issue
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*",
+    callback = function()
+        if vim.o.filetype == "" then
+            vim.opt_local.filetype = "text"
+        end
+    end,
+})
 vim.o.lazyredraw = true
