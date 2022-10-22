b1feb783d7a6eaa25b5e708ef1281b286dbf2814
Stark-X <xiaojiezhi2@gmail.com>
2022-10-22 17:39:47 +0800

    [nvim] convert init.vim to init.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,16 @@
+-- advise from NvimTree disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.runtimepath:prepend("~/.vim")
+vim.opt.runtimepath:append("~/.vim/after")
+
+vim.cmd([[let &packpath = &runtimepath]])
+vim.cmd([[source ~/.vimrc]])
+vim.cmd([[colorscheme vim-monokai-tasty]])
+
+vim.o.lazyredraw = true
+vim.g.python3_host_prog = "$HOME/.pyenv/versions/neovim/bin/python"
+
+require("plugins")
+require("neovide")
