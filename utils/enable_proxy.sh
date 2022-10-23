146914338fbf2f40c3346e9100163caeb2a76c06
Stark-X <13653522+Stark-X@users.noreply.github.com>
2022-10-23 14:40:21 +0800

    [util] dynamic change the proxy host for different platform

--- utils/enable_proxy.sh
+++ utils/enable_proxy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-proxy_prefix="https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890"
+proxy_host="127.0.0.1:7890"
+
+if [[ -f ~/.proxy_host ]];then
+    proxy_host=`cat ~/.proxy_host`
+fi
+
+proxy_prefix="https_proxy=http://${proxy_host} http_proxy=http://${proxy_host} all_proxy=socks5://${proxy_host}"
 
 cmdline=$@
 if [[ ! -z ${cmdline} ]]; then
