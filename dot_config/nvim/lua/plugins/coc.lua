18d3284f1f6b790b7cff7aadd2c6792cf4715320
Stark-X <13653522+Stark-X@users.noreply.github.com>
2023-04-06 14:38:20 +0800

    [coc] drop duplicated tabline, statusline settings

--- dot_config/nvim/lua/plugins/coc.lua
+++ dot_config/nvim/lua/plugins/coc.lua
@@ -254,13 +254,10 @@ api.nvim_create_user_command(
 
 -- coc-symbol-line https://github.com/xiyaowong/coc-symbol-line
 function _G.symbol_line()
-  local curwin = vim.g.statusline_winid or 0
-  local curbuf = vim.api.nvim_win_get_buf(curwin)
-  local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, 'coc_symbol_line')
-  return ok and line or ''
+    local curwin = vim.g.statusline_winid or 0
+    local curbuf = vim.api.nvim_win_get_buf(curwin)
+    local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, "coc_symbol_line")
+    return ok and line or ""
 end
 
-vim.o.tabline = '%!v:lua.symbol_line()'
-vim.o.statusline = '%!v:lua.symbol_line()'
-vim.o.winbar = '%!v:lua.symbol_line()'
-
+vim.o.winbar = "%!v:lua.symbol_line()"
