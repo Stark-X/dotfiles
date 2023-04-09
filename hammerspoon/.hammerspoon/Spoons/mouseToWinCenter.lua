87a9b9be648517483239a2472e777e5badebd3db
Stark-X <xiaojiezhi2@gmail.com>
2023-04-09 22:14:59 +0800

    [hammerspoon] extract spoon mouseToWinCenter

---
+++ hammerspoon/.hammerspoon/Spoons/mouseToWinCenter.lua
@@ -0,0 +1,43 @@
+-- find the mouse
+function mouseHighlight()
+    if not mouseCircle == nil then
+        mouseCircle:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
+    end
+    mousepoint = hs.mouse.getRelativePosition()
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x - 40, mousepoint.y - 40, 80, 80))
+    mouseCircle:setStrokeColor({ ["red"] = 1, ["blue"] = 0, ["green"] = 0, ["alpha"] = 1 })
+    mouseCircle:setFill(false)
+    mouseCircle:setStrokeWidth(5)
+    mouseCircle:show()
+
+    mouseCircleTimer = hs.timer.doAfter(0.3, function() mouseCircle:delete() end)
+end
+
+eventtapOneClick = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }, function(e)
+    -- logger.d(e:getProperty(hs.eventtap.event.properties["mouseEventClickState"]))
+    b_fn = hs.eventtap.checkKeyboardModifiers()["fn"]
+    if b_fn and e:getProperty(hs.eventtap.event.properties["mouseEventClickState"]) == 1 then
+        local win = hs.window.focusedWindow()
+        local frame = win:frame()
+
+        local target = {
+            x = frame.x + frame.w / 2,
+            y = frame.y + frame.h / 2,
+        }
+        -- logger.d(target.x, target.y)
+        -- logger.d(frame.x, frame.y)
+        hs.mouse.setAbsolutePosition(target)
+
+        mouseHighlight()
+        return true
+    else
+        return false
+    end
+end)
+
+return {
+    init = function() eventtapOneClick:start() end,
+}
--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -157,48 +157,6 @@ hs.hotkey.bind({ "cmd", "alt" }, "right", function()
     win:moveOneScreenEast()
 end)
 
--- find the mouse
-function mouseHighlight()
-    if not mouseCircle == nil then
-        mouseCircle:delete()
-        if mouseCircleTimer then
-            mouseCircleTimer:stop()
-        end
-    end
-    mousepoint = hs.mouse.getRelativePosition()
-    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x - 40, mousepoint.y - 40, 80, 80))
-    mouseCircle:setStrokeColor({ ["red"] = 1, ["blue"] = 0, ["green"] = 0, ["alpha"] = 1 })
-    mouseCircle:setFill(false)
-    mouseCircle:setStrokeWidth(5)
-    mouseCircle:show()
-
-    mouseCircleTimer = hs.timer.doAfter(0.3, function() mouseCircle:delete() end)
-end
-
--- move mouse though windows
-eventtapOneClick = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }, function(e)
-    -- logger.d(e:getProperty(hs.eventtap.event.properties["mouseEventClickState"]))
-    b_fn = hs.eventtap.checkKeyboardModifiers()["fn"]
-    if b_fn and e:getProperty(hs.eventtap.event.properties["mouseEventClickState"]) == 1 then
-        local win = hs.window.focusedWindow()
-        local frame = win:frame()
-
-        local target = {
-            x = frame.x + frame.w / 2,
-            y = frame.y + frame.h / 2,
-        }
-        -- logger.d(target.x, target.y)
-        -- logger.d(frame.x, frame.y)
-        hs.mouse.setAbsolutePosition(target)
-
-        mouseHighlight()
-        return true
-    else
-        return false
-    end
-end)
-eventtapOneClick:start()
-
 -- interactive window resize
 hs.grid.setGrid("7x5", nil, nil)
 hs.grid.setMargins({ 0, 0 })
@@ -217,6 +175,7 @@ wf_keep_awake:subscribe(wf.windowUnfocused, function()
     hs.caffeine.set("displayIdle", false)
 end)
 
+require("./Spoons/mouseToWinCenter").init()
 require("./Spoons/winDrag").init()
 require("./Spoons/vimEmulation").init({
     "MacVim",
