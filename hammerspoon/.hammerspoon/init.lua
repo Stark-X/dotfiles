5257f5164ec4ff280ac8330c0245c22fa28023c3
Stark-X <xiaojiezhi2@gmail.com>
2023-04-09 22:11:47 +0800

    [hammerspoon] extract spoon for vim emulation

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -206,33 +206,7 @@ hs.hotkey.bind({ "shift" }, "space", function() hs.grid.show() end)
 
 REPEAT_DELAY = 200
 
--- Delete a word before the cursor
-fn_delete_a_word = function() hs.eventtap.keyStroke('{"alt", }', "delete", REPEAT_DELAY) end
-bn_delete_a_word = hs.hotkey.bind('{"ctrl",}', "w", fn_delete_a_word, nil, fn_delete_a_word)
-
-function enableBinds()
-    bn_delete_a_word:enable()
-    bn_up:enable()
-    bn_down:enable()
-    bn_left:enable()
-    bn_right:enable()
-    bn_esc:enable()
-end
-function disableBinds()
-    bn_delete_a_word:disable()
-    bn_up:disable()
-    bn_down:disable()
-    bn_left:disable()
-    bn_right:disable()
-    bn_esc:disable()
-end
-
 local wf = hs.window.filter
-
-wf_vim = wf.new({ "MacVim", "iTerm2", "PhpStorm", "IntelliJ IDEA", "PyCharm", "WebStorm", "Code", "tmux", "neovide" })
-wf_vim:subscribe(wf.windowFocused, disableBinds)
-wf_vim:subscribe(wf.windowUnfocused, enableBinds)
-
 wf_keep_awake = wf.new({ "谜底时钟" })
 wf_keep_awake:subscribe(wf.windowFocused, function()
     _GUtils.tips("Start Caffeine")
@@ -243,25 +217,15 @@ wf_keep_awake:subscribe(wf.windowUnfocused, function()
     hs.caffeine.set("displayIdle", false)
 end)
 
--- TODO: Check-> Warning:   wfilter: No accessibility access to app GoldenDict (no watcher pid)
--- all_wf = wf.new(true)
--- all_wf:subscribe(wf.windowFocused, function(win, name, en) logger.d(win, name) end)
-
--- Map ctrl + [ to escape
-fn_escape = function() hs.eventtap.keyStroke(nil, "escape") end
-bn_esc = hs.hotkey.bind('{"ctrl",}', "[", fn_escape, nil, fn_escape)
-
--- simulate vim mode
-fn_left = function() hs.eventtap.keyStroke(nil, "left", REPEAT_DELAY) end
-bn_left = hs.hotkey.bind('{"ctrl",}', "h", fn_left, nil, fn_left)
-
-fn_down = function() hs.eventtap.keyStroke(nil, "down", REPEAT_DELAY) end
-bn_down = hs.hotkey.bind('{"ctrl",}', "j", fn_down, nil, fn_down)
-
-fn_up = function() hs.eventtap.keyStroke(nil, "up", REPEAT_DELAY) end
-bn_up = hs.hotkey.bind('{"ctrl",}', "k", fn_up, nil, fn_up)
-
-fn_right = function() hs.eventtap.keyStroke(nil, "right", REPEAT_DELAY) end
-bn_right = hs.hotkey.bind('{"ctrl",}', "l", fn_right, nil, fn_right)
-
 require("./Spoons/winDrag").init()
+require("./Spoons/vimEmulation").init({
+    "MacVim",
+    "iTerm2",
+    "PhpStorm",
+    "IntelliJ IDEA",
+    "PyCharm",
+    "WebStorm",
+    "Code",
+    "tmux",
+    "neovide",
+})
