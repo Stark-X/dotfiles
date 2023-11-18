7d5582c495bbc19d277b5692e87735617f7e39ad
Stark-X <13653522+Stark-X@users.noreply.github.com>
2023-11-18 22:50:42 +0800

    [zshrc] upgrade fzf

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,14 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
+  PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
+source "${HOME}/.fzf/shell/completion.zsh"
 
 # Key bindings
 # ------------
 source "${HOME}/.fzf/shell/key-bindings.zsh"
-
