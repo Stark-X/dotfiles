c5472c7f9f11925f2aab4436d96ea29d01c22302
Stark-X <contact-me@stark-x.cn>
2024-04-06 10:58:27 +0800

    🔨 chore(bash): update fzf settings by fzf upgrade

--- bash/.fzf.bash
+++ bash/.fzf.bash
@@ -1,13 +1,8 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/jzxiao/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/jzxiao/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/Users/jzxiao/.fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/Users/jzxiao/.fzf/shell/key-bindings.bash"
+# Set up fzf key bindings and fuzzy completion
+eval "$(fzf --bash)"
