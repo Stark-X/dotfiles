a2bb74c340f76def90a287842e0619b5482fc96a
Stark-X <contact-me@stark-x.cn>
2024-04-06 10:58:27 +0800

    🔨 chore(bash): update fzf settings by fzf upgrade

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "${HOME}/.fzf/shell/completion.zsh"
-
-# Key bindings
-# ------------
-source "${HOME}/.fzf/shell/key-bindings.zsh"
+eval "$(fzf --zsh)"
