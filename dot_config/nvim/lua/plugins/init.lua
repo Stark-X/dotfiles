823f08c808571e26a7663ea472fe259f26721453
Stark-X <13653522+Stark-X@users.noreply.github.com>
2024-06-21 18:11:27 +0800

    ✨ feat(dot_config): !! replace Packer to lazy.nvim for neovim plugins management

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+-- vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+-- vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
 return {
     { "folke/which-key.nvim", event = "VeryLazy" },
