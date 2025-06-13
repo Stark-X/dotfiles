d8fae83b4aa5962e49c1d4569e6086a7dd8c7c8b
Stark-X <contact-me@stark-x.cn>
2025-06-13 13:59:59 +0800

    [self] fix boot-sys_dep

---
+++ .config/yadm/boot-sys_dep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+linux_init() {
+  if ! locale-check en_US.UTF-8; then
+    sudo locale-gen en_US.UTF-8
+  fi
+
+  sudo apt install unzip zip traceroute dnsutils silversearcher-ag bison gcc lua5.4 make telnet
+}
+
+system_type=$(uname -s)
+
+if [[  "$system_type" = "Linux" || "$system_type" = "WSL" ]]; then
+  linux_init
+fi
+
+# vim: set ft=bash
