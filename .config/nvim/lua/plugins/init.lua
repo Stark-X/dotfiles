a926e69f9c532b3f6a49bcbc174d86821b86cc0b
Stark-X <contact-me@stark-x.cn>
2025-07-31 10:14:10 +0800

    [neovim] replace argtextobj with 'wellle/targets.vim'

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -205,7 +205,8 @@ return {
     -- cx{range} to swap two text-obj, X for select mode
     "tommcdo/vim-exchange",
     "machakann/vim-highlightedyank",
-    "vim-scripts/argtextobj.vim",
+    -- vim-scripts/argtextobj.vim alternative
+    "wellle/targets.vim",
     -- toggle quickfix list with <learder>q and toggle location list with <leader>l
     "Valloric/ListToggle",
     -- + expand_region_expand
